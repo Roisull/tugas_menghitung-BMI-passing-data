44dc94851d682527e88467a33791688327ab117d
lusiro <lusiro2001@students.amikom.ac.id>
2022-09-04 17:39:39 +0700

    finish

--- app/src/main/java/com/example/hitungbmi/passingDataBmi/IntentActivity.kt
+++ app/src/main/java/com/example/hitungbmi/passingDataBmi/IntentActivity.kt
@@ -1,7 +1,6 @@
 package com.example.hitungbmi.passingDataBmi
 
-import android.annotation.SuppressLint
-import android.app.Person
+import com.example.hitungbmi.dataClass.Person
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
@@ -15,7 +14,6 @@ class IntentActivity : AppCompatActivity() {
     lateinit var tvHasilBmi: TextView
     lateinit var tvHasilKategori: TextView
 
-    @SuppressLint("NewApi")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intent)
@@ -65,9 +63,93 @@ class IntentActivity : AppCompatActivity() {
             }else{
                 tvHasilKategori.text = "Obesitas kelas 3"
             }
+
         }else if (pePerson != null){
+            tvHasilUmur.setText(dataPerson.umurPerson.trim())
+            tvHasilTinggi.setText(dataPerson.tinggiBadanPerson.trim())
+            tvHasilBerat.setText(dataPerson.beratBadanPerson.trim())
+            val tinggiToDouble = tvHasilTinggi.toString().toDouble()
+            val beratToDouble = tvHasilBerat.toString().toDouble()
+            val tinggiToM = tinggiToDouble / 100
+
+            val totalBmi = beratToDouble / (tinggiToM * tinggiToM)
+            tvHasilBmi.text = totalBmi.toString().trim()
+
+            if (totalBmi < 15.9){
+                tvHasilKategori.text = "Terlalu Kurus"
+            }else if (totalBmi in 16.0..17.0){
+                tvHasilKategori.text = "Cukup Kurus"
+            }else if (totalBmi in 17.1..18.5){
+                tvHasilKategori.text = "Sedikit Kurus"
+            }else if (totalBmi in 18.6..24.9){
+                tvHasilKategori.text = "Normal"
+            }else if (totalBmi in 25.0..30.0){
+                tvHasilKategori.text = "Gemuk"
+            }else if (totalBmi in 30.1..35.0){
+                tvHasilKategori.text = "Obesitas kelas 1"
+            }else if (totalBmi in 35.1..40.0){
+                tvHasilKategori.text = "Obesitas kelas 2"
+            }else{
+                tvHasilKategori.text = "Obesitas kelas 3"
+            }
+
+        }else if (pePersonpar != null){
+            tvHasilUmur.text = dataPersonPar.umurPerson
+            tvHasilTinggi.text = dataPersonPar.tinggiPerson
+            tvHasilBerat.text = dataPersonPar.beratPerson
+
+            val tinggiToDouble = tvHasilTinggi.toString().toDouble()
+            val beratToDouble = tvHasilBerat.toString().toDouble()
+            val tinggiToM = tinggiToDouble / 100
+
+            val totalBmi = beratToDouble / (tinggiToM * tinggiToM)
+            tvHasilBmi.text = totalBmi.toString().trim()
+            if (totalBmi < 15.9){
+                tvHasilKategori.text = "Terlalu Kurus"
+            }else if (totalBmi in 16.0..17.0){
+                tvHasilKategori.text = "Cukup Kurus"
+            }else if (totalBmi in 17.1..18.5){
+                tvHasilKategori.text = "Sedikit Kurus"
+            }else if (totalBmi in 18.6..24.9){
+                tvHasilKategori.text = "Normal"
+            }else if (totalBmi in 25.0..30.0){
+                tvHasilKategori.text = "Gemuk"
+            }else if (totalBmi in 30.1..35.0){
+                tvHasilKategori.text = "Obesitas kelas 1"
+            }else if (totalBmi in 35.1..40.0){
+                tvHasilKategori.text = "Obesitas kelas 2"
+            }else{
+                tvHasilKategori.text = "Obesitas kelas 3"
+            }
 
         }else{
+            tvHasilUmur.setText(intent.getStringExtra("umur"))
+            tvHasilTinggi.setText(intent.getStringExtra("tinggi"))
+            tvHasilBerat.setText(intent.getStringExtra("berat"))
+
+            val tinggiToDouble = tvHasilTinggi.toString().toDouble()
+            val beratToDouble = tvHasilBerat.toString().toDouble()
+            val tinggiToM = tinggiToDouble / 100
+
+            val totalBmi = beratToDouble / (tinggiToM * tinggiToM)
+            tvHasilBmi.text = totalBmi.toString().trim()
+            if (totalBmi < 15.9){
+                tvHasilKategori.text = "Terlalu Kurus"
+            }else if (totalBmi in 16.0..17.0){
+                tvHasilKategori.text = "Cukup Kurus"
+            }else if (totalBmi in 17.1..18.5){
+                tvHasilKategori.text = "Sedikit Kurus"
+            }else if (totalBmi in 18.6..24.9){
+                tvHasilKategori.text = "Normal"
+            }else if (totalBmi in 25.0..30.0){
+                tvHasilKategori.text = "Gemuk"
+            }else if (totalBmi in 30.1..35.0){
+                tvHasilKategori.text = "Obesitas kelas 1"
+            }else if (totalBmi in 35.1..40.0){
+                tvHasilKategori.text = "Obesitas kelas 2"
+            }else{
+                tvHasilKategori.text = "Obesitas kelas 3"
+            }
 
         }
     }
